1a96dbf9a39683d379e343ad19bc02c691c181d6
Aaron Feingold <ajfeingold88@gmail.com>
2020-05-29 01:57:22 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
 require 'pry'
 
 def oxford_comma(array)
-  until array.length <= 1
+  while array.length <= 1
    return array.join(" and " )
+ end
     return array[0..-2].join(", ") + ", and " + array[-1] if array.length >1
 end
 
