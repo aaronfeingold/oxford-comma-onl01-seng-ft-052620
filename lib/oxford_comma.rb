09e36628f7dc2fd12b8605bbfde514b656ec49b8
Aaron Feingold <ajfeingold88@gmail.com>
2020-05-29 02:16:15 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,9 @@ def oxford_comma(array)
   
   # return array.join(" and ") if array.length <= 1
   
-   
-    return array[0..-2].join(", ") + ", and " + array[-1] if array.length > 1
-  
+   if array.length > 1
+    return array[0..-2].join(", ") + ", and " + array[-1] 
+  end 
 end
 
 # def english_join(array = nil)
