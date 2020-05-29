2cac16c1ea1dba1525915763a17af7a88d3f1d31
Aaron Feingold <ajfeingold88@gmail.com>
2020-05-29 02:26:16 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,7 +5,7 @@ def oxford_comma(array)
     return array
   elsif array.length > 1
     return array[0..-2].join(", ") + ", and " + array[-1]
-  else
+  elsif array.length == 1 
     return array.join(" and ")
   end 
 end
