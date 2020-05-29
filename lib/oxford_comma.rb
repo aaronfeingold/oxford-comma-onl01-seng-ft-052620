248f91338f5bee69a67800fb42b7e8b26545e328
Aaron Feingold <ajfeingold88@gmail.com>
2020-05-29 01:51:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,8 @@
 require 'pry'
 
-def oxford_comma(array = nil)
+def oxford_comma(array)
    return array.join if array.length <= 1
       array[0..-2].join(", ") + ", and " + array[-1]
-    
 end
 
 # def english_join(array = nil)
