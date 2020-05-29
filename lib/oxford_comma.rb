20e8a4f1b053eefda89b96da4c6d57d5907cb3fd
Aaron Feingold <ajfeingold88@gmail.com>
2020-05-29 01:45:54 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,17 +1,12 @@
 require 'pry'
 
 def oxford_comma(array)
-  
-    while array.length < 2
-      return array.join(" and ")
-    end
-    #while array.length > 1 
-     # return array.join(", ")
-   # end
-  
+   return array.to_s if array.nil? or array.length <= 1
+  array[0..-2].join(", ") + " and " + array[-1]
+    
 end
 
 # def english_join(array = nil)
  # return array.to_s if array.nil? or array.length <= 1
-  # array[0..-2].join(", ") + " and " + array[-1]
-# end
+ # array[0..-2].join(", ") + " and " + array[-1]
+ # end
