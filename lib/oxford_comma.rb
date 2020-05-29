37dc530ac0e2a91aa4b476e88620482cea2f06b2
Aaron Feingold <ajfeingold88@gmail.com>
2020-05-29 02:25:30 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,10 +1,8 @@
 require 'pry'
 
 def oxford_comma(array)
-   
-   if array.length  == 0 
+  if array.length  == 0 
     return array
-    
   elsif array.length > 1
     return array[0..-2].join(", ") + ", and " + array[-1]
   else
