3fee4badf0f34d1485fc2652e03bae1b54181950
Aaron Feingold <ajfeingold88@gmail.com>
2020-05-29 02:06:20 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -2,7 +2,7 @@ require 'pry'
 
 def oxford_comma(array)
   
-  return array.to_s if array.length <= 1
+  return array.join(" and ") if array.length <= 1
  
   return array[0..-2].join(", ") + ", and " + array[-1] if array.length >1
   
